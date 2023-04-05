59b6238fe7da9667aed30560a0124677467ffca2
worthowski <66230461+umitcangungor@users.noreply.github.com>
2023-04-06 00:26:52 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,65 @@
+print("Merhaba Etiya") 
+print("Hoş geldiniz")
+
+# yorum
+
+# değişkenler -start
+# metinsel, numerik, obje
+
+# string
+text = "Merhaba"
+
+print(text)
+print(text)
+print(text)
+text = "Selam"
+print(text)
+print(text)
+
+studentCount = 45 # int, integer => tam sayı
+print(studentCount + 5)
+
+averagePoint = 25.5 
+print(averagePoint + 5)
+
+isVerified = True # bool, boolean => True veya False
+print(isVerified)
+
+print(type(text))
+print(type(studentCount))
+print(type(averagePoint))
+print(type(isVerified))
+# değişkenler -end
+
+# operatörler -start
+number = 10
+# matematiksel operatörler 
+print(10 + 10)
+print(number + number)
+
+print(number - 5)
+
+print(number / 2)
+
+print(number * 5)
+
+# mod operatörü => sol tarafdaki değerin sağ taraftaki değere bölümünden kalan sonuç
+print(number % 3)
+
+
+print(number + ((10 - number) * (5 / 10)))
+
+# mantıksal operatörler - karşılaştırma operatörleri
+print(10 == 10) #true
+print(11 == 10) #false
+
+print(10 != 10) #false
+print(11 != 10) #true
+
+print(number > 10) #false
+print(number >= 10) #true
+
+print(number < 10) #false
+print(number <= 10) #true
+
+# operatörler -end
